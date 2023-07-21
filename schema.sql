2f489ef46507de120a56c1912fbd6663d93d2515
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-07-21 05:43:51 +0500

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,28 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 	ADD species TEXT;
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name TEXT,
+    age INT,
+    primary key(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    primary key(id)
+);
+
+ALTER TABLE animals 
+	ADD primary key(id);
+
+ALTER TABLE animals 
+	DROP COLUMN species;
+
+ALTER TABLE animals
+    ADD species_id INT REFERENCES species(id);
+
+ALTER TABLE animals
+    ADD owner_id INT REFERENCES owners(id);
